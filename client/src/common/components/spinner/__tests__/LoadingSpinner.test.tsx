221262673d3f8d3a9e49f361116813767db24ccd
Jori Lindell <jori.lindell@gmail.com>
2019-12-02 17:46:25 +0200

    Add loading spinner tests

---
+++ client/src/common/components/spinner/__tests__/LoadingSpinner.test.tsx
@@ -0,0 +1,23 @@
+import { shallow } from "enzyme";
+import React from "react";
+
+import LoadingSpinner from "../LoadingSpinner";
+
+it("LoadingSpinner matches snapshot", () => {
+  const spinner = shallow(<LoadingSpinner isLoading={true} />);
+  expect(spinner.html()).toMatchSnapshot();
+});
+
+it("render spinner if isLoading is true", () => {
+  const spinner = shallow(<LoadingSpinner isLoading={true} />).children();
+  expect(spinner.prop("className")).toContain("spinner");
+});
+
+it("render child component if isLoading is false", () => {
+  const spinner = shallow(
+    <LoadingSpinner isLoading={false}>
+      <div className="component"></div>
+    </LoadingSpinner>
+  ).children();
+  expect(spinner.prop("className")).toEqual("component");
+});
