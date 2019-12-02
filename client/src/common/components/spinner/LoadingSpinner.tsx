c038261880249096217d7ca887e355267c2f563b
Jori Lindell <jori.lindell@gmail.com>
2019-12-02 16:35:21 +0200

    Loading spinner component

---
+++ client/src/common/components/spinner/LoadingSpinner.tsx
@@ -0,0 +1,22 @@
+import React, { FunctionComponent } from "react";
+
+import styles from "./loadingSpinner.module.scss";
+
+const LoadingSpinner: FunctionComponent<{ isLoading: boolean }> = ({
+  isLoading,
+  children
+}) => {
+  return (
+    <>
+      {isLoading ? (
+        <div className={styles.spinnerWrapper}>
+          <div className={styles.spinner} />
+        </div>
+      ) : (
+        children
+      )}
+    </>
+  );
+};
+
+export default LoadingSpinner;
--- client/src/domain/event/EventPageContainer.tsx
+++ client/src/domain/event/EventPageContainer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 
+import LoadingSpinner from "../../common/components/spinner/LoadingSpinner";
 import { useEventDetailsQuery } from "../../generated/graphql";
 import Layout from "../app/layout/Layout";
 import EventHero from "./EventHero";
+import styles from "./eventPage.module.scss";
 
 const EventPageContainer: React.FC<
   RouteComponentProps<{ id: string }>
@@ -15,12 +17,15 @@ const EventPageContainer: React.FC<
 
   return (
     <Layout>
-      {eventData && (
-        <>
-          <EventHero eventData={eventData} />
-        </>
-      )}
-      {loading && "LADATAAN..."}
+      <div className={styles.eventPageWrapper}>
+        <LoadingSpinner isLoading={loading}>
+          {eventData && (
+            <>
+              <EventHero eventData={eventData} />
+            </>
+          )}
+        </LoadingSpinner>
+      </div>
     </Layout>
   );
 };
