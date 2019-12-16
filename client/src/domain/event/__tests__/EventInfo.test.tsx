4074c75064a203e68919ae06324211813ecb19ed
Jori Lindell <jori.lindell@gmail.com>
2019-12-16 14:29:53 +0200

    Update tests

--- client/src/domain/event/__tests__/EventInfo.test.tsx
+++ client/src/domain/event/__tests__/EventInfo.test.tsx
@@ -1,11 +1,33 @@
+import { MockedProvider } from "@apollo/react-testing";
 import * as React from "react";
 import renderer from "react-test-renderer";
 
+import { OrganizationDetailsDocument } from "../../../generated/graphql";
 import { mockEventData } from "../constants";
 import EventInfo from "../EventInfo";
 
+const mocks = [
+  {
+    request: {
+      query: OrganizationDetailsDocument,
+      variables: {
+        id: "provider:123"
+      }
+    },
+    result: {
+      data: {
+        dog: { id: "1", name: "Buck" }
+      }
+    }
+  }
+];
+
 test("EventInfo matches snapshot", () => {
-  const component = renderer.create(<EventInfo eventData={mockEventData} />);
+  const component = renderer.create(
+    <MockedProvider mocks={mocks}>
+      <EventInfo eventData={mockEventData} />
+    </MockedProvider>
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
