a9b424046b941c6dbe7ef1041623dfdb485f1190
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-21 09:18:53 +0200

    Hide buy ticket button for free events in search

---
+++ src/domain/eventSearch/__tests__/EventCard.tests.jsx
@@ -0,0 +1,47 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { MemoryRouter } from "react-router";
+
+import EventCard from "../EventCard";
+
+const getWrapper = props =>
+  render(
+    <MemoryRouter>
+      <EventCard {...props} />
+    </MemoryRouter>
+  );
+
+test("should show buy button when event has an offer", () => {
+  const mockEvent = {
+    images: [],
+    name: {},
+    offers: [
+      {
+        infoUrl: {
+          fi: "https://example.domain"
+        }
+      }
+    ]
+  };
+  const { queryByText } = getWrapper({ event: mockEvent });
+
+  expect(queryByText("Osta liput")).not.toEqual(null);
+});
+
+test("should hide buy button when event is free", () => {
+  const mockEvent = {
+    images: [],
+    name: {},
+    offers: [
+      {
+        infoUrl: {
+          fi: "https://example.domain"
+        },
+        isFree: true
+      }
+    ]
+  };
+  const { queryByText } = getWrapper({ event: mockEvent });
+
+  expect(queryByText("Osta liput")).toEqual(null);
+});
