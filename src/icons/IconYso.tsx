394746e662e00740ef51a2a1467ad2ddede081e4
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-21 08:48:21 +0200

    Fix errors caused by YSO icon

--- src/icons/IconYso.tsx
+++ src/icons/IconYso.tsx
@@ -11,19 +11,19 @@ export default ({ className = "" }: Props) => (
     xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g transform="translate(-20.000000, -197.000000)">
         <g transform="translate(0.000000, 186.000000)">
           <g transform="translate(20.000000, 11.000000)">
             <polygon
               stroke="#000000"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               points="0.75 0.75 10.5 0.75 23.25 13.5 13.5 23.25 0.75 10.5"
             ></polygon>
             <circle
               fill="#000000"
-              fill-rule="nonzero"
+              fillRule="nonzero"
               cx="6"
               cy="6"
               r="1.5"
