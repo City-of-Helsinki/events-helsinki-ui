f269938e0f3d114c542ae61b3c5f71817d275167
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-21 09:46:48 +0200

    Hide buy ticket button for free events in event hero

--- src/domain/event/__tests__/EventHero.test.tsx
+++ src/domain/event/__tests__/EventHero.test.tsx
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import * as React from "react";
 import { MemoryRouter } from "react-router";
 import renderer from "react-test-renderer";
@@ -13,6 +14,33 @@ test("EventHero matches snapshot", () => {
   );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getWrapper = (props: any) =>
+  render(
+    <MemoryRouter>
+      <EventHero {...props} />
+    </MemoryRouter>
+  );
+
+test("should hide buy button for free events", () => {
+  const freeEventMockData = {
+    ...mockEventData,
+    eventDetails: {
+      ...mockEventData.eventDetails,
+      offers: [
+        {
+          ...mockEventData.eventDetails.offers[0],
+          infoUrl: "some-url",
+          isFree: true
+        }
+      ]
+    }
+  };
+  const { queryByText } = getWrapper({ eventData: freeEventMockData });
+
+  expect(queryByText("Osta lippu")).toEqual(null);
 });
 
 export {};
