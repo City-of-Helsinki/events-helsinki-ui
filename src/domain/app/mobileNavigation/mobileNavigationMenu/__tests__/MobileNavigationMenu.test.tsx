d0c063fb10b8b53bbd89e5a4d0d30b9488a64ffe
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-22 11:19:13 +0200

    Deprecate i18nInit

--- src/domain/app/mobileNavigation/mobileNavigationMenu/__tests__/MobileNavigationMenu.test.tsx
+++ src/domain/app/mobileNavigation/mobileNavigationMenu/__tests__/MobileNavigationMenu.test.tsx
@@ -1,15 +1,11 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { I18nextProvider } from "react-i18next";
 
-import i18 from "../../../../../common/translation/i18n/i18nInit";
 import MobileNavigationMenu from "../MobileNavigationMenu";
 
 it("MobileNavigationMenu matches snapshot", () => {
   const container = shallow(
-    <I18nextProvider i18n={i18}>
-      <MobileNavigationMenu isMenuOpen={true} onMenuClose={() => {}} />
-    </I18nextProvider>
+    <MobileNavigationMenu isMenuOpen={true} onMenuClose={() => {}} />
   );
   expect(container.html()).toMatchSnapshot();
 });
