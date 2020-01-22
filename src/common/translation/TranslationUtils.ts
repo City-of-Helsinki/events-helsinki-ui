9c7cb41763d39a900b59f69d2d27dbec897e7d4d
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-22 11:13:01 +0200

    Deprecate changeLanguage

--- src/common/translation/TranslationUtils.ts
+++ src/common/translation/TranslationUtils.ts
@@ -1,10 +1,5 @@
 import { i18n as I18nInstanceType } from "i18next";
 
-import i18n from "./i18n/i18nInit";
-
-export const changeLanguage = (language: string) =>
-  i18n.changeLanguage(language);
-
 /**
  * Safety check in case
  * somehow i18n have problem or have empty languages.
