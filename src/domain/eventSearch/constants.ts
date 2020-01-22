332c69e92e20dadbab5127794e9cf935dd68e9ef
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-22 14:04:34 +0200

    Include additional keywords for category culture search
    These were defined by the project manager. Further documentation can be
    found from TH-226.

--- src/domain/eventSearch/constants.ts
+++ src/domain/eventSearch/constants.ts
@@ -11,3 +11,31 @@ export enum EVENT_SORT_OPTIONS {
   START_TIME = "start_time",
   START_TIME_DESC = "-start_time"
 }
+
+// Finnish keys are used for keywords that do not have an official English
+// translation. An approximate translation has been provided on the comment row.
+export const CULTURE_KEYWORDS = {
+  "Elokuva ja media": "kulke:205", // Films and Media
+  Sirkus: "kulke:51", // Circus
+  Teatteri: "kulke:33", // theatre
+  "Teatteri ja sirkus": "kulke:351", // Theatre and Circus
+  art: "yso:p2851",
+  "art exhibitions": "yso:p6889",
+  "art museums": "yso:p8144",
+  "cinema (art forms)": "yso:p16327",
+  "contemporary art": "yso:p9593",
+  "contemporary dance": "yso:p10105",
+  "cultural events": "yso:p360",
+  "dance (performing arts)": "yso:p1278",
+  exhibitions: "yso:p5121",
+  films: "yso:p1235",
+  "fine arts": "yso:p2739",
+  "literary art": "yso:p7969",
+  literature: "yso:p8113",
+  "modern art": "yso:p9592",
+  museums: "yso:p4934",
+  music: "yso:p1808",
+  "performing arts": "yso:p2850",
+  teatteri: "matko:teatteri", // theatre
+  theatre: "yso:p2625" // in Finnish teatteritaide, "theatre arts"
+};
