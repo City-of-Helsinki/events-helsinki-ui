764219d0255baff4114df3271fb2e917a298d49b
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-23 12:54:44 +0200

    Remove i18next-node-fs-backend
    To my understanding, this dependency is not useful because we are
    transpiling our server code.

--- src/common/translation/i18n/init.server.tsx
+++ src/common/translation/i18n/init.server.tsx
@@ -1,29 +1,12 @@
-import fs from "fs";
 import i18next from "i18next";
 import i18nextMiddleware from "i18next-express-middleware";
-import I18nextFsBackend from "i18next-node-fs-backend";
-import path from "path";
 import { initReactI18next } from "react-i18next";
 
 import config from "./config";
 
-const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = (relativePath: string) =>
-  path.resolve(appDirectory, relativePath);
-const appSrc = resolveApp("src");
-
-const extendedConfig = {
-  ...config,
-  backend: {
-    addPath: appSrc + "/common/translation/i18n/{{lng}}.missing.json",
-    loadPath: appSrc + "/common/translation/i18n/{{lng}}.json"
-  }
-};
-
 i18next
   .use(initReactI18next)
-  .use(I18nextFsBackend)
   .use(i18nextMiddleware.LanguageDetector)
-  .init(extendedConfig);
+  .init(config);
 
 export default i18next;
