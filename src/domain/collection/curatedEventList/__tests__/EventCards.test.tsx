de47214be025b0b169643f2ea07a5adaa1d4d11a
Jori Lindell <jori.lindell@gmail.com>
2020-01-27 18:29:43 +0200

    Add spanshot test to EventCards component

--- src/domain/collection/curatedEventList/__tests__/EventCards.test.tsx
+++ src/domain/collection/curatedEventList/__tests__/EventCards.test.tsx
@@ -1,44 +1,25 @@
 import { MockedProvider } from "@apollo/react-testing";
 import pretty from "pretty";
-import React from "react";
+import * as React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import { MemoryRouter } from "react-router";
 import wait from "waait";
 
-import { EventListDocument } from "../../../../generated/graphql";
+import { EventDetailsDocument } from "../../../../generated/graphql";
 import { mockEventData } from "../../../event/constants";
-import { EVENT_SORT_OPTIONS, PAGE_SIZE } from "../../../eventSearch/constants";
-import { getEventFilters } from "../../../eventSearch/EventListUtils";
-import { mockCollection } from "../../constants";
-import EventList from "../EventList";
+import EventCards from "../EventCards";
 
-window.scrollTo = jest.fn();
-
-const searchParams = new URLSearchParams(
-  mockCollection.collectionDetails.eventListQuery
-);
 const mocks = [
   {
     request: {
-      query: EventListDocument,
-      variables: getEventFilters(
-        searchParams,
-        PAGE_SIZE,
-        EVENT_SORT_OPTIONS.END_TIME
-      )
+      query: EventDetailsDocument,
+      variables: {
+        id: mockEventData.eventDetails.id
+      }
     },
     result: {
-      data: {
-        eventList: {
-          data: [mockEventData.eventDetails],
-          meta: {
-            count: 1,
-            next: null,
-            previous: null
-          }
-        }
-      }
+      data: mockEventData
     }
   }
 ];
@@ -62,7 +43,7 @@ test("EventList should match snapshot", async () => {
     render(
       <MockedProvider mocks={mocks} addTypename={false}>
         <MemoryRouter>
-          <EventList collectionData={mockCollection} />
+          <EventCards eventIds={[mockEventData.eventDetails.id]} />
         </MemoryRouter>
       </MockedProvider>,
       container
