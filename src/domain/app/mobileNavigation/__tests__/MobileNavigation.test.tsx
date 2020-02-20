637d8f2177579a52afda8226b1bd0e459e402b01
Jori Lindell <jori.lindell@gmail.com>
2020-02-20 15:20:43 +0200

    Update tests

--- src/domain/app/mobileNavigation/__tests__/MobileNavigation.test.tsx
+++ src/domain/app/mobileNavigation/__tests__/MobileNavigation.test.tsx
@@ -1,8 +1,18 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { MemoryRouter } from "react-router";
+import routeData, { MemoryRouter } from "react-router";
 
 import MobileNavigation from "../MobileNavigation";
+
+const mockLocation = {
+  hash: "",
+  pathname: "/fi/home",
+  search: "",
+  state: ""
+};
+beforeEach(() => {
+  jest.spyOn(routeData, "useLocation").mockReturnValue(mockLocation);
+});
 
 it("MobileNavigation matches snapshot", () => {
   const container = shallow(
