2b30f0b7cf63069d7dabc05e0a5e313e3e373c5b
Jori Lindell <jori.lindell@gmail.com>
2020-02-21 17:18:19 +0200

    Move getEventIdFromUrl function to event utils

--- src/domain/event/EventUtils.ts
+++ src/domain/event/EventUtils.ts
@@ -53,6 +53,18 @@ export const getEventDistrict = (
 };
 
 /**
+ * Get event id from url
+ * @param {string} url
+ * @return {string}
+ */
+export const getEventIdFromUrl = (url: string): string | null => {
+  const trimmedUrl = url.replace(/\?(.*)/, "");
+  const eventId = trimmedUrl.match(/event\/(.*)/);
+
+  return eventId && eventId.length ? eventId[1].replace("/", "") : null;
+};
+
+/**
  * Get event price as a string
  * @param {object} event
  * @param {string} locale
