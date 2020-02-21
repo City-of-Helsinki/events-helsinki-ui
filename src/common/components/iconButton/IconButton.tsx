e8e13f2b16ec536a8c1d65b936f63b7704a61cb4
Jori Lindell <jori.lindell@gmail.com>
2020-02-21 17:16:37 +0200

    Create IconButton to unify buttons with icon

---
+++ src/common/components/iconButton/IconButton.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import React from "react";
+
+import styles from "./iconButton.module.scss";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  ariaLabel: string;
+  icon: React.ReactElement;
+  size: "default" | "small";
+}
+
+const IconButton: React.FC<Props> = ({
+  ariaLabel,
+  icon,
+  size,
+  type = "button",
+  ...rest
+}) => {
+  return (
+    <button
+      aria-label={ariaLabel}
+      className={classNames(styles.iconButton, styles[size])}
+      {...rest}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default IconButton;
