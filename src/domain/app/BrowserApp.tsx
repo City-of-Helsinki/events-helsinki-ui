3550da06a11fa4abfc4abd42bb55ddda718185a9
Jori Lindell <jori.lindell@gmail.com>
2020-03-10 17:16:30 +0200

    Set language info correctly to application

--- src/domain/app/BrowserApp.tsx
+++ src/domain/app/BrowserApp.tsx
@@ -9,6 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import createGraphqlClient from "../../util/createGraphqlClient";
 import AppRoutes from "./AppRoutes";
+import CommonMeta from "./CommonMeta";
 
 const BrowserApp: FunctionComponent = () => {
   useSSR(window.initialI18nStore, window.initialLanguage);
@@ -20,6 +21,7 @@ const BrowserApp: FunctionComponent = () => {
     <BrowserRouter>
       <ApolloProvider client={client}>
         <ApolloHooksProvider client={client}>
+          <CommonMeta />
           <AppRoutes />
         </ApolloHooksProvider>
       </ApolloProvider>
