76947efff25e86d10d70f52b80f562fcf26276e7
Jori Lindell <jori.lindell@gmail.com>
2020-03-16 15:46:59 +0200

    Send graphql errors to sentry

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,6 @@ if (process.env.REACT_APP_SENTRY_ENVIRONMENT) {
 
 ReactDOM.hydrate(<BrowserApp />, document.getElementById("root"));
 
-console.log(process.env);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
