13f8a9337ab69887a9d84c831860618147a93faa
Jori Lindell <jori.lindell@gmail.com>
2020-03-16 14:01:44 +0200

    Initialize Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import "./assets/styles/main.scss";
 
+import * as Sentry from "@sentry/browser";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import BrowserApp from "./domain/app/BrowserApp";
 import * as serviceWorker from "./serviceWorker";
 
+if (process.env.REACT_APP_SENTRY_ENVIRONMENT) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
+    release: `${process.env.REACT_APP_APPLICATION_NAME}@${process.env.REACT_APP_VERSION}`
+  });
+}
+
 ReactDOM.hydrate(<BrowserApp />, document.getElementById("root"));
 
+console.log(process.env);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
