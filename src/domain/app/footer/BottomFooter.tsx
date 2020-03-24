ddbceeacc0d1f453f3a35aa545f3dd55b1748393
Jori Lindell <jori.lindell@gmail.com>
2020-03-24 16:37:36 +0200

    Fix about and feedback links

--- src/domain/app/footer/BottomFooter.tsx
+++ src/domain/app/footer/BottomFooter.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
+import useLocale from "../../../hooks/useLocale";
 import Container from "../layout/Container";
 import styles from "./bottomFooter.module.scss";
 
 const BottomFooter: React.FC = () => {
   const { t } = useTranslation();
+  const locale = useLocale();
 
   return (
     <div className={styles.bottomFooter}>
       <Container>
         <div className={styles.contentWrapper}>
           <div className={styles.linkContainer}>
-            <Link to="about">{t("footer.linkAbout")}</Link>
-            <Link to="feedback">{t("footer.linkFeedback")}</Link>
+            <Link to={`/${locale}/about`}>{t("footer.linkAbout")}</Link>
+            <Link to={`/${locale}/feedback`}>{t("footer.linkFeedback")}</Link>
           </div>
           <div className={styles.logoWrapper}>
             <div className={styles.helsinkiLogo}></div>
