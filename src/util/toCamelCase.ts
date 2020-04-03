8333ea12701b31572efbd73b2f84fdc0218e79ee
Jori Lindell <jori.lindell@gmail.com>
2020-04-03 11:24:50 +0300

    Fix unhandled error when trying to call toCamelCase with empty string

--- src/util/toCamelCase.ts
+++ src/util/toCamelCase.ts
@@ -5,11 +5,11 @@
  * event_end_date => eventEndDate
  */
 export default (snakecase: string) => {
-  return (
-    snakecase[0].toLowerCase() +
-    snakecase
-      .substr(1)
-      .toLowerCase()
-      .replace(/(_[a-z])/g, $1 => $1.toUpperCase().replace("_", ""))
-  );
+  return !!snakecase && snakecase.length
+    ? snakecase[0].toLowerCase() +
+        snakecase
+          .substr(1)
+          .toLowerCase()
+          .replace(/(_[a-z])/g, $1 => $1.toUpperCase().replace("_", ""))
+    : "";
 };
