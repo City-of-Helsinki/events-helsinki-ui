a3d99db6981b07475b20c8efd966489c671fc862
Jori Lindell <jori.lindell@gmail.com>
2020-08-11 18:04:54 +0300

    Update SrOnly component tests

--- src/common/components/srOnly/__tests__/SrOnly.test.tsx
+++ src/common/components/srOnly/__tests__/SrOnly.test.tsx
@@ -1,51 +1,21 @@
-import pretty from 'pretty';
+import { render } from '@testing-library/react';
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
 
 import SrOnly from '../SrOnly';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let container: any = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it('SrOnly component type should be div', () => {
-  act(() => {
-    render(
-      <SrOnly className="test">
-        <div>Test</div>
-      </SrOnly>,
-      container
-    );
-  });
-  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
-    "<div class=\\"srOnly test\\">
+it('matches snapshot', () => {
+  const { container, rerender } = render(
+    <SrOnly className="test">
       <div>Test</div>
-    </div>"
-  `);
-});
-
-it('SrOnly component type should be span', () => {
-  act(() => {
-    render(
-      <SrOnly as="span">
-        <div>Test</div>
-      </SrOnly>,
-      container
-    );
-  });
-  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(
-    `"<span class=\\"srOnly\\"><div>Test</div></span>"`
+    </SrOnly>
   );
+
+  expect(container.firstChild).toMatchSnapshot();
+
+  rerender(
+    <SrOnly as="span">
+      <div>Test</div>
+    </SrOnly>
+  );
+  expect(container.firstChild).toMatchSnapshot();
 });
