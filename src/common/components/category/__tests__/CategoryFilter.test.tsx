5c05af8915d7a3bcd13e067ca11f5a2a49d59fac
Jori Lindell <jori.lindell@gmail.com>
2020-08-11 16:46:56 +0300

    Update tests for CategoryFilter component

--- src/common/components/category/__tests__/CategoryFilter.test.tsx
+++ src/common/components/category/__tests__/CategoryFilter.test.tsx
@@ -1,17 +1,42 @@
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { IconHome } from 'hds-react';
 import React from 'react';
 
 import CategoryFilter from '../CategoryFilter';
 
-it('CategoryFilter matched snapshot', () => {
-  const el = shallow(
+const category = {
+  text: 'text',
+  value: 'value',
+};
+
+it('matches snapshot', () => {
+  const { container } = render(
     <CategoryFilter
       icon={<IconHome />}
-      onClick={() => {}}
-      text={'test'}
-      value={'value'}
+      onClick={jest.fn()}
+      text={category.text}
+      value={category.value}
     />
   );
-  expect(el.html()).toMatchSnapshot();
+
+  expect(container).toMatchSnapshot();
+});
+
+it('calls onClick callback when catetgory filter button is clicked ', () => {
+  const onClickMock = jest.fn();
+  render(
+    <CategoryFilter
+      icon={<IconHome />}
+      onClick={onClickMock}
+      text={category.text}
+      value={category.value}
+    />
+  );
+
+  expect(screen.queryByText(category.text)).toBeInTheDocument();
+
+  userEvent.click(screen.getByText(category.text));
+
+  expect(onClickMock).toHaveBeenCalled();
 });
