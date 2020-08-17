18bb880fdbb1f81886098ef6f6971246cae32e15
Jori Lindell <jori.lindell@gmail.com>
2020-08-17 17:15:59 +0300

    Add hyphenation to collection card title and prevent container content to overflow

--- src/domain/app/layout/Container.tsx
+++ src/domain/app/layout/Container.tsx
@@ -3,7 +3,11 @@ import React, { FunctionComponent } from 'react';
 import styles from './container.module.scss';
 
 const Container: FunctionComponent = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default Container;
