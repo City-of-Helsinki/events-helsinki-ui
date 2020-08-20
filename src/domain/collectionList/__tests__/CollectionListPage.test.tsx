1946d4dd750db92934004801517b788ee4d8d60c
Jori Lindell <jori.lindell@gmail.com>
2020-08-20 21:38:30 +0300

    Hide expired collections from collection list

--- src/domain/collectionList/__tests__/CollectionListPage.test.tsx
+++ src/domain/collectionList/__tests__/CollectionListPage.test.tsx
@@ -2,18 +2,34 @@ import { screen } from '@testing-library/react';
 import * as React from 'react';
 
 import translations from '../../../common/translation/i18n/fi.json';
-import { CollectionListDocument } from '../../../generated/graphql';
+import {
+  CollectionFieldsFragment,
+  CollectionListDocument,
+} from '../../../generated/graphql';
 import { render } from '../../../util/testUtils';
 import mockCollection from '../../collection/__mocks__/collection';
 import CollectionListPage from '../CollectionListPage';
 
-const collections = ['1', '2', '3', '4', '5', '6', '7'].map(id => ({
+const collections: CollectionFieldsFragment[] = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+].map(id => ({
   ...mockCollection,
   __typename: 'CollectionDetails',
   id,
 }));
 
-const mocks = [
+const linkText = translations.commons.collectionCard.ariaLabelLink.replace(
+  '{{title}}',
+  mockCollection.title.fi || ''
+);
+
+const getMocks = (collections: CollectionFieldsFragment[]) => [
   {
     request: {
       query: CollectionListDocument,
@@ -30,8 +46,42 @@ const mocks = [
 ];
 
 test('matches snapshot', async () => {
+  const mocks = getMocks(collections);
   const { container } = render(<CollectionListPage />, { mocks });
 
   await screen.findByText(translations.collectionList.title);
+
+  expect(screen.getAllByRole('link', { name: linkText }).length).toBe(7);
+
   expect(container.firstChild).toMatchSnapshot();
+});
+
+test('should not show collection if language is not supported', async () => {
+  const mocks = getMocks(
+    collections.map((collection, index) =>
+      Object.assign(
+        {},
+        collection,
+        index % 2 ? { title: { ...collection.title, fi: '' } } : {}
+      )
+    )
+  );
+  render(<CollectionListPage />, { mocks });
+
+  await screen.findByText(translations.collectionList.title);
+
+  expect(screen.getAllByRole('link', { name: linkText }).length).toBe(4);
+});
+
+test('should not show expired collections', async () => {
+  const mocks = getMocks(
+    collections.map((collection, index) =>
+      Object.assign({}, collection, { expired: Boolean(index % 2) })
+    )
+  );
+  render(<CollectionListPage />, { mocks });
+
+  await screen.findByText(translations.collectionList.title);
+
+  expect(screen.getAllByRole('link', { name: linkText }).length).toBe(4);
 });
