4ee4ef20f4ea8a70dea16473ac58561826f5b793
Jori Lindell <jori.lindell@gmail.com>
2020-08-20 20:58:37 +0300

    Refactor BrowserApp tests

--- src/domain/app/__tests__/BrowserApp.test.tsx
+++ src/domain/app/__tests__/BrowserApp.test.tsx
@@ -1,18 +1,15 @@
-import { MockedProvider } from '@apollo/react-testing';
 import { act } from '@testing-library/react';
-import { mount } from 'enzyme';
 import i18n from 'i18next';
 import React from 'react';
-import { MemoryRouter } from 'react-router';
 import wait from 'waait';
 
 import {
   CollectionListDocument,
   LandingPagesDocument,
 } from '../../../generated/graphql';
+import { render } from '../../../util/testUtils';
 import mockCollection from '../../collection/__mocks__/collection';
 import mockLandingPage from '../../landingPage/__mocks__/landingPage';
-import App from '../App';
 import AppRoutes from '../AppRoutes';
 
 const mocks = [
@@ -52,14 +49,8 @@ export async function actWait(amount = 0) {
   });
 }
 
-const wrapperCreator = (route: string) =>
-  mount(
-    <MockedProvider mocks={mocks} addTypename={true}>
-      <MemoryRouter initialEntries={[route]}>
-        <AppRoutes />
-      </MemoryRouter>
-    </MockedProvider>
-  );
+const getWrapper = (route: string) =>
+  render(<AppRoutes />, { mocks, routes: [route] });
 
 beforeEach(() => {
   act(() => {
@@ -68,40 +59,32 @@ beforeEach(() => {
 });
 
 it('user from supported locale will be redirect to App with that locale', async () => {
-  const wrapper = wrapperCreator('/en/');
+  getWrapper('/en/');
   await actWait();
-  const app = wrapper.find(App);
 
-  expect(app).toBeDefined();
-  expect(app.props().match.params.locale).toEqual('en');
+  expect(i18n.language).toEqual('en');
 });
 
 it('user from unsupported locale prefix will be redirect to route with support prefix', async () => {
-  const wrapper = wrapperCreator('/vi/');
+  const { history } = getWrapper('/vi/');
   await actWait();
 
-  const app = wrapper.find(App);
-
-  expect(app.props().match.params.locale).toEqual('fi');
-  expect(app.props().location.pathname).toContain('/fi/vi/');
+  expect(i18n.language).toEqual('fi');
+  expect(history.location.pathname).toContain('/fi/vi/');
 });
 
 it('user without locale prefix will be redirect to route with support prefix', async () => {
-  const wrapper = wrapperCreator('/foo-url');
+  const { history } = getWrapper('/foo-url');
   await actWait();
 
-  const app = wrapper.find(App);
-
-  expect(app.props().match.params.locale).toEqual('fi');
-  expect(app.props().location.pathname).toContain('/fi/foo-url');
+  expect(i18n.language).toEqual('fi');
+  expect(history.location.pathname).toContain('/fi/foo-url');
 });
 
 it('user with route with unsupport locale will be redirect to App anyway, with supported locale', async () => {
-  const wrapper = wrapperCreator('/dk/foo');
+  const { history } = getWrapper('/dk/foo');
   await actWait();
 
-  const app = wrapper.find(App);
-
-  expect(app.props().match.params.locale).toEqual('fi');
-  expect(app.props().location.pathname).toContain('/fi/dk/foo');
+  expect(i18n.language).toEqual('fi');
+  expect(history.location.pathname).toContain('/fi/dk/foo');
 });
