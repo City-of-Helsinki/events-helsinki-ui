59bccb99104290c2fba309b99d562630b16543f2
Jori Lindell <jori.lindell@gmail.com>
2020-10-06 17:11:58 +0300

    Improve EventClosedHero component tests

--- src/domain/event/eventClosedHero/__tests__/EventClosedHero.test.tsx
+++ src/domain/event/eventClosedHero/__tests__/EventClosedHero.test.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-import { render } from '../../../../util/testUtils';
+import translations from '../../../../common/translation/i18n/fi.json';
+import { render, screen, userEvent } from '../../../../util/testUtils';
 import EventClosedHero from '../EventClosedHero';
 
-it('matches snapshot', () => {
-  const { container } = render(<EventClosedHero />);
+it('should render all text fields', () => {
+  render(<EventClosedHero />);
 
-  expect(container.firstChild).toMatchSnapshot();
+  expect(
+    screen.queryByRole('heading', {
+      name: translations.event.hero.titleEventClosed,
+    })
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(translations.event.hero.textEventClosed)
+  ).toBeInTheDocument();
+  expect(
+    screen.getByRole('button', {
+      name: translations.event.hero.buttonToHomePage,
+    })
+  ).toBeInTheDocument();
+});
+
+it('should go to home page when clicking button', () => {
+  const { history } = render(<EventClosedHero />);
+
+  userEvent.click(
+    screen.getByRole('button', {
+      name: translations.event.hero.buttonToHomePage,
+    })
+  );
+  expect(history.location.pathname).toBe('/fi/home');
 });
