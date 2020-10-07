323976eaf298a88ca1813bd73a2e9cbda6b3b8de
Jori Lindell <jori.lindell@gmail.com>
2020-10-07 12:51:15 +0300

    Improve SimilarEvents tests

--- src/domain/event/similarEvents/__tests__/SimilarEvents.test.tsx
+++ src/domain/event/similarEvents/__tests__/SimilarEvents.test.tsx
@@ -1,7 +1,8 @@
+import { clear } from 'console';
 import { advanceTo } from 'jest-date-mock';
 import React from 'react';
-import wait from 'waait';
 
+import translations from '../../../../common/translation/i18n/fi.json';
 import {
   EventFieldsFragment,
   EventListDocument,
@@ -11,7 +12,7 @@ import {
   fakeEvents,
   fakeKeywords,
 } from '../../../../util/mockDataUtils';
-import { act, render, screen } from '../../../../util/testUtils';
+import { render, screen, waitFor } from '../../../../util/testUtils';
 import SimilarEvents from '../SimilarEvents';
 
 const keywordIds = ['yso:1', 'yso:2'];
@@ -34,12 +35,12 @@ const variables = {
   startsAfter: undefined,
   superEventType: ['umbrella', 'none'],
 };
-
+const keywords = fakeKeywords(
+  keywordIds.length,
+  keywordIds.map((id) => ({ id, name: { fi: id } }))
+).data;
 const event = fakeEvent({
-  keywords: fakeKeywords(
-    keywordIds.length,
-    keywordIds.map((id) => ({ id }))
-  ).data,
+  keywords,
 }) as EventFieldsFragment;
 const events = fakeEvents(3);
 const eventsResponse = { data: { eventList: events } };
@@ -54,13 +55,30 @@ const mocks = [
   },
 ];
 
+afterAll(() => {
+  clear();
+});
+
 test('should render similar event cards', async () => {
   advanceTo(new Date('2020-08-11'));
   render(<SimilarEvents event={event} />, { mocks });
 
-  await act(wait);
+  await waitFor(() => {
+    expect(
+      screen.queryByRole('heading', {
+        name: translations.event.similarEvents.title,
+      })
+    ).toBeInTheDocument();
+  });
 
   events.data.forEach((event) => {
-    expect(screen.queryByText(event.name.fi as string)).toBeInTheDocument();
+    expect(
+      screen.queryByRole('link', {
+        name: translations.commons.eventCard.ariaLabelLink.replace(
+          '{{name}}',
+          event.name.fi
+        ),
+      })
+    ).toBeInTheDocument();
   });
 });
