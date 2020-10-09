d23830dc2ebdafc9ff91cd523482292b3b1357ea
Jori Lindell <jori.lindell@gmail.com>
2020-10-09 14:17:10 +0300

    Remove extra container

--- src/domain/app/header/Header.tsx
+++ src/domain/app/header/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Container from '../layout/Container';
 import styles from './header.module.scss';
 import MobileNavigation from './mobileNavigation/MobileNavigation';
 import Navbar from './navbar/Navbar';
@@ -8,9 +7,7 @@ import Navbar from './navbar/Navbar';
 const Header: React.FC = () => {
   return (
     <header className={styles.headerWrapper}>
-      <Container>
-        <Navbar />
-      </Container>
+      <Navbar />
       <MobileNavigation />
     </header>
   );
