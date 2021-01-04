3e0155d9cc4b08c3ba42aa996d5be5dd1219e804
Jaakko Nenonen <jaakko@codelicious.fi>
2021-01-04 20:24:43 +0200

    th-914: run ci testcafe tests on locally deployed server

--- testcafe/utils/settings.ts
+++ testcafe/utils/settings.ts
@@ -1,6 +1,6 @@
 const LOCAL_ENV_URL = 'http://localhost:3000';
 
 export const getEnvUrl = (path = ''): string => {
-  const baseUrl = process.env.SERVICE_URL ?? LOCAL_ENV_URL;
+  const baseUrl = process.env.BROWSER_TESTS_LOCAL_ENV_URL ?? LOCAL_ENV_URL;
   return `${baseUrl}${path?.startsWith('/') ? path : `/${path ?? ''}`}`;
 };
