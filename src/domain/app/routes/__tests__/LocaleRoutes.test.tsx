6cc24e5d312319c7f270aaf046d20a07466f9c5e
Santeri Rusila <santtu_1993@hotmail.com>
2021-01-04 07:24:42 +0200

    Add test for deprecated /event/:id route redirection

--- src/domain/app/routes/__tests__/LocaleRoutes.test.tsx
+++ src/domain/app/routes/__tests__/LocaleRoutes.test.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 
-import { render } from '../../../../util/testUtils';
+import { render, renderWithRoute, waitFor } from '../../../../util/testUtils';
+import { ROUTES } from '../constants';
 import LocaleRoutes from '../LocaleRoutes';
 
 const AppWithRouter = withRouter(LocaleRoutes);
 
 it('renders without crashing', () => {
   render(<AppWithRouter />);
+});
+
+it('redirects deprecated /event/:id to /events/:id', async () => {
+  const id = '123';
+  const { history } = renderWithRoute(<AppWithRouter />, {
+    routes: [`/fi/event/${id}`],
+    path: '/:locale/event/:id',
+  });
+
+  await waitFor(() => {
+    expect(history.location.pathname).toBe(
+      `/fi${ROUTES.EVENT.replace(':id', id)}`
+    );
+  });
 });
