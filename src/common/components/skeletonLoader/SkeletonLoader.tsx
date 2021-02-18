f0e634a7e404d264e3800725a8df86879525e568
Ivan Melnik <melnik.job@gmail.com>
2021-02-18 15:49:04 +0200

    Improves skeleton loader

--- src/common/components/skeletonLoader/SkeletonLoader.tsx
+++ src/common/components/skeletonLoader/SkeletonLoader.tsx
@@ -1,17 +1,29 @@
 import React, { FunctionComponent } from 'react';
 import ContentLoader from 'react-content-loader';
 
-const SkeletonLoader: FunctionComponent = () => {
+interface Props {
+  width?: string;
+  height?: string;
+  backgroundColor?: string;
+  foregroundColor?: string;
+}
+
+const SkeletonLoader: FunctionComponent<Props> = ({
+  width = '200',
+  height = '18',
+  backgroundColor = '#f3f3f3',
+  foregroundColor = '#ecebeb',
+}) => {
   return (
     <ContentLoader
       speed={2}
-      width={'100%'}
-      height={18}
-      viewBox="0 0 100% 18"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#ecebeb"
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      backgroundColor={backgroundColor}
+      foregroundColor={foregroundColor}
     >
-      <rect x="0" y="0" rx="3" ry="3" width="100%" height="18" />
+      <rect x="0" y="0" rx="3" ry="3" width={width} height={height} />
     </ContentLoader>
   );
 };
