1081c1769c385458678abe868eac746fa2992c20
Santeri Rusila <santtu_1993@hotmail.com>
2021-02-19 18:26:37 +0200

    Add tests for pagination in CuratdEventList

--- src/domain/collection/curatedEventList/__tests__/CuratedEventList.test.tsx
+++ src/domain/collection/curatedEventList/__tests__/CuratedEventList.test.tsx
@@ -1,4 +1,6 @@
 import { advanceTo, clear } from 'jest-date-mock';
+import chunk from 'lodash/chunk';
+import range from 'lodash/range';
 import React from 'react';
 
 import translations from '../../../../common/translation/i18n/fi.json';
@@ -9,7 +11,7 @@ import {
 } from '../../../../generated/graphql';
 import { fakeCollection, fakeEvents } from '../../../../util/mockDataUtils';
 import { render, screen, userEvent, waitFor } from '../../../../util/testUtils';
-import CuratedEventList from '../CuratedEventList';
+import CuratedEventList, { PAGE_SIZE } from '../CuratedEventList';
 
 const eventIds = ['1', '2', '3', '4', '5'];
 const curatedEvents = eventIds.map(
@@ -21,18 +23,60 @@ const collection = fakeCollection({
   curatedEvents,
 }) as CollectionFieldsFragment;
 
-const getMocks = (events: EventFieldsFragment[]) => [
+const getMocks = (events: EventFieldsFragment[], ids = eventIds) => [
   {
     request: {
       query: EventsByIdsDocument,
       variables: {
-        ids: eventIds,
+        ids,
         include: ['location'],
       },
     },
     result: { data: { eventsByIds: events } },
   },
 ];
+
+const getMocksForPagination = (eventsCount = 35) => {
+  const eventIds = range(eventsCount).map((i) => (i + 1).toString());
+  const eventNames = eventIds.map((id) => `Event ${id}`);
+  const curatedEvents = eventIds.map(
+    (id) => `http://localhost:3000/fi/event/${id}`
+  );
+
+  const chunkedEventIds = chunk(eventIds, PAGE_SIZE);
+  const chunkedEvents = chunk(eventNames, PAGE_SIZE).map(
+    (splittedEventNames, chunkIndex) => {
+      return fakeEvents(
+        splittedEventNames.length,
+        splittedEventNames.map((event, index) => ({
+          endTime: '2020-12-12',
+          startTime: '2020-12-12',
+          id: chunkedEventIds[chunkIndex][index],
+          name: { fi: event },
+        }))
+      );
+    }
+  );
+
+  const mocks = chunkedEventIds.map((eventIds, index) => {
+    return getMocks(
+      chunkedEvents[index].data as EventFieldsFragment[],
+      eventIds
+    )[0];
+  });
+
+  const collection = fakeCollection({
+    curatedEvents,
+  }) as CollectionFieldsFragment;
+
+  return { mocks, collection, eventNames };
+};
+
+const waitForRequestToComplete = async () => {
+  await waitFor(() => {
+    expect(screen.queryByTestId('loading-spinner')).not.toBeInTheDocument();
+  });
+};
 
 afterAll(() => {
   clear();
@@ -103,4 +147,40 @@ test('should show expired events', async () => {
   eventNames.forEach((eventName) => {
     expect(screen.getByText(eventName)).toBeInTheDocument();
   });
+});
+
+test('event list pagination works', async () => {
+  advanceTo('2020-10-05');
+  const eventsCount = 35;
+  const { collection, mocks, eventNames } = getMocksForPagination(eventsCount);
+  render(<CuratedEventList collection={collection} />, {
+    mocks,
+  });
+  await waitForRequestToComplete();
+
+  const clickShowMoreEventsButton = (name: RegExp) => {
+    userEvent.click(screen.getByRole('button', { name, hidden: true }));
+  };
+
+  for (
+    let eventsFetchedCount = PAGE_SIZE;
+    eventsFetchedCount < eventsCount;
+    eventsFetchedCount += PAGE_SIZE
+  ) {
+    clickShowMoreEventsButton(
+      new RegExp(
+        `Näytä lisää tapahtumia \\(${eventsCount - eventsFetchedCount}\\)`,
+        'i'
+      )
+    );
+    await waitForRequestToComplete();
+  }
+
+  expect(
+    screen.queryByRole('button', { name: /näytä lisää tapahtumia/ })
+  ).not.toBeInTheDocument();
+
+  for (const eventName of eventNames) {
+    expect(screen.queryByText(eventName)).toBeInTheDocument();
+  }
 });
