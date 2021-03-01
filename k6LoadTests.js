5d85e9c8b16c6e87d258caabef82a2d9dc281c18
Ivan Melnik <melnik.job@gmail.com>
2021-03-01 14:21:26 +0200

    slack notifications added

--- k6LoadTests.js
+++ k6LoadTests.js
@@ -3,16 +3,16 @@ import { sleep } from 'k6';
 import http from 'k6/http';
 
 export const options = {
-  duration: '1m',
-  vus: 1,
+  duration: '10m',
+  vus: 20,
   thresholds: {
     //avg is around 800ms on https://tapahtumat.test.kuva.hel.ninja
-    http_req_duration: ['p(95)<1'],
+    http_req_duration: ['p(95)<5000'],
   },
 };
 
 export default () => {
   const res = http.get('https://tapahtumat.test.kuva.hel.ninja/fi/events');
   //10 loads per minute
-  sleep(1);
+  sleep(6);
 };
