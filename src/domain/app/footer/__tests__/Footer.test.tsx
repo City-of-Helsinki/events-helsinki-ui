989ab3887c982414bdb3edbad115b7ad0099c4ec
Santeri Rusila <santtu_1993@hotmail.com>
2021-03-01 13:14:50 +0200

    Add courses link to footer

--- src/domain/app/footer/__tests__/Footer.test.tsx
+++ src/domain/app/footer/__tests__/Footer.test.tsx
@@ -1,10 +1,37 @@
 import * as React from 'react';
 
-import { render } from '../../../../test/testUtils';
+import { render, screen, userEvent } from '../../../../test/testUtils';
+import { ROUTES } from '../../routes/constants';
 import Footer from '../Footer';
 
 test('matches snapshot', () => {
   const { container } = render(<Footer />);
 
   expect(container.firstChild).toMatchSnapshot();
+});
+
+test.only('clicking links should route to right place', () => {
+  const { history } = render(<Footer />);
+  const pushSpy = jest.spyOn(history, 'push');
+
+  const testValues = [
+    {
+      linkName: 'Tapahtumat',
+      path: `/fi${ROUTES.EVENTS}`,
+    },
+    {
+      linkName: 'Harrastukset',
+      path: `/fi${ROUTES.COURSES}`,
+    },
+    {
+      linkName: 'Suosittelemme',
+      path: `/fi${ROUTES.COLLECTIONS}`,
+    },
+  ];
+
+  for (const { linkName, path } of testValues) {
+    userEvent.click(screen.getByRole('link', { name: linkName }));
+    expect(pushSpy).toHaveBeenCalledWith(path);
+    pushSpy.mockReset();
+  }
 });
