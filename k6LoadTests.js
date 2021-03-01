df1afb9ff256ac0bf2f16995951daa652db752cb
Ivan Melnik <melnik.job@gmail.com>
2021-03-01 14:01:31 +0200

    slack notification test

--- k6LoadTests.js
+++ k6LoadTests.js
@@ -7,7 +7,7 @@ export const options = {
   vus: 20,
   thresholds: {
     //avg is around 800ms on https://tapahtumat.test.kuva.hel.ninja
-    http_req_duration: ['p(95)<5000'],
+    http_req_duration: ['p(95)<1'],
   },
 };
 
