77e0b23510392be2697b28a621b8b1b6e96647dd
Ivan Melnik <melnik.job@gmail.com>
2021-03-09 09:58:34 +0200

    TH-1006: Add local event time to event hero (#231)
    * improvements
    
    * fix tests

--- src/domain/event/eventHero/__tests__/EventHero.test.tsx
+++ src/domain/event/eventHero/__tests__/EventHero.test.tsx
@@ -194,7 +194,7 @@ test('Register button should be visible and clickable', () => {
   expect(global.open).toBeCalledWith(registrationUrl);
 });
 
-test('should have event dates when super event is not defined', () => {
+test('should show event dates when super event is not defined', () => {
   const mockEvent = getFakeEvent();
 
   render(<EventHero event={mockEvent} eventType="course" />);
@@ -209,7 +209,7 @@ test('should have event dates when super event is not defined', () => {
   expect(screen.getByText(dateStr)).toBeInTheDocument();
 });
 
-test('should have super event dates when super event is defined', () => {
+test('should show event dates and super event dates if super event is defined', () => {
   const mockEvent = getFakeEvent();
   const mockSuperEvent = getFakeEvent({
     startTime: '2020-06-22T07:00:00.000000Z',
@@ -231,4 +231,14 @@ test('should have super event dates when super event is defined', () => {
     timeAbbreviation: translations.commons.timeAbbreviation,
   });
   expect(screen.getByText(superDateStr)).toBeInTheDocument();
+
+  const dateStr = getDateRangeStr({
+    start: mockEvent.startTime,
+    end: mockEvent.endTime,
+    locale: 'fi',
+    includeTime: true,
+    timeAbbreviation: translations.commons.timeAbbreviation,
+  });
+
+  expect(screen.getByText(dateStr)).toBeInTheDocument();
 });
