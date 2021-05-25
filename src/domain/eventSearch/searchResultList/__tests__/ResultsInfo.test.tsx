65a3f141d475cf21f9131b9791b049508f8f9248
Niko Lindroos <niko.j.makela@gmail.com>
2021-05-25 12:25:39 +0300

    Added some tests for ResultsInfo component.
    TH-1075 Added text tests for few results query.

--- src/domain/eventSearch/searchResultList/__tests__/ResultsInfo.test.tsx
+++ src/domain/eventSearch/searchResultList/__tests__/ResultsInfo.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
-import { render, screen } from '../../../../test/testUtils';
+import { render, screen, userEvent, waitFor } from '../../../../test/testUtils';
+import { ROUTES } from '../../../app/routes/constants';
 import ResultsInfo from '../ResultsInfo';
 
-test('matches snapshot', () => {
+test('events with 0 results matches snapshot for no results', () => {
   const { container } = render(
     <ResultsInfo resultsCount={0} eventType="event" />
   );
@@ -11,8 +12,27 @@ test('matches snapshot', () => {
   expect(container).toMatchSnapshot();
 });
 
-test('renders no events found text and hobby search button', () => {
-  render(<ResultsInfo resultsCount={0} eventType="event" />);
+test('events with 1 results matches snapshot of few results', () => {
+  const { container } = render(
+    <ResultsInfo resultsCount={1} eventType="event" />
+  );
+
+  expect(container).toMatchSnapshot();
+});
+
+test('events with 5 results matches snapshot of normal results', () => {
+  const { container } = render(
+    <ResultsInfo resultsCount={5} eventType="event" />
+  );
+
+  expect(container).toMatchSnapshot();
+});
+
+test('renders no events found text and hobby search button', async () => {
+  const { history } = render(
+    <ResultsInfo resultsCount={0} eventType="event" />
+  );
+  const historyPush = jest.spyOn(history, 'push');
 
   const texts = [
     'Valitsemillasi hakuehdoilla ei löytynyt yhtään tapahtumaa',
@@ -23,13 +43,20 @@ test('renders no events found text and hobby search button', () => {
     expect(screen.queryByText(text)).toBeInTheDocument();
   });
 
-  expect(
+  userEvent.click(
     screen.queryByRole('button', { name: 'Siirry hakemaan harrastuksia' })
-  ).toBeInTheDocument();
+  );
+
+  await waitFor(() => {
+    expect(historyPush).toHaveBeenCalledWith(`/fi${ROUTES.COURSES}`);
+  });
 });
 
-test('renders no courses found text and event search button', () => {
-  render(<ResultsInfo resultsCount={0} eventType="course" />);
+test('renders no courses found text and event search button', async () => {
+  const { history } = render(
+    <ResultsInfo resultsCount={0} eventType="course" />
+  );
+  const historyPush = jest.spyOn(history, 'push');
 
   const texts = [
     'Valitsemillasi hakuehdoilla ei löytynyt yhtään harrastusta',
@@ -40,9 +67,67 @@ test('renders no courses found text and event search button', () => {
     expect(screen.queryByText(text)).toBeInTheDocument();
   });
 
-  expect(
+  userEvent.click(
     screen.queryByRole('button', { name: 'Siirry hakemaan tapahtumia' })
-  ).toBeInTheDocument();
+  );
+
+  await waitFor(() => {
+    expect(historyPush).toHaveBeenCalledWith(`/fi${ROUTES.EVENTS}`);
+  });
 });
 
-// TODO: add more test for different cases (0 < resultsCount < 5) + that action buttons work correctly
+test.each([1, 4])(
+  'renders few events found text and hobby search button when event count is %i',
+  async (resultsCount) => {
+    const { history } = render(
+      <ResultsInfo resultsCount={resultsCount} eventType="event" />
+    );
+    const historyPush = jest.spyOn(history, 'push');
+
+    const texts = [
+      'Hakuehdoillasi löytyi vain vähän tapahtumia.',
+      'Valitsemillasi hakuehdoilla löytyi vain vähän tapahtumia. ' +
+        'Kokeile muuttaa hakuehtoja tai siirry harrastusten hakuun.',
+    ];
+
+    texts.forEach((text) => {
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
+
+    userEvent.click(
+      screen.queryByRole('button', { name: 'Siirry hakemaan harrastuksia' })
+    );
+
+    await waitFor(() => {
+      expect(historyPush).toHaveBeenCalledWith(`/fi${ROUTES.COURSES}`);
+    });
+  }
+);
+
+test.each([1, 4])(
+  'renders few courses found text and event search button when course count is %i',
+  async (resultsCount) => {
+    const { history } = render(
+      <ResultsInfo resultsCount={resultsCount} eventType="course" />
+    );
+    const historyPush = jest.spyOn(history, 'push');
+
+    const texts = [
+      'Valitsemillasi hakuehdoilla löytyi vain vähän hakutuloksia.',
+      'Valitsemillasi hakuehdoilla löytyi vain vähän harrastuksia. ' +
+        'Kokeile muuttaa hakuehtoja tai siirry tapahtumien hakuun.',
+    ];
+
+    texts.forEach((text) => {
+      expect(screen.queryByText(text)).toBeInTheDocument();
+    });
+
+    userEvent.click(
+      screen.queryByRole('button', { name: 'Siirry hakemaan tapahtumia' })
+    );
+
+    await waitFor(() => {
+      expect(historyPush).toHaveBeenCalledWith(`/fi${ROUTES.EVENTS}`);
+    });
+  }
+);
