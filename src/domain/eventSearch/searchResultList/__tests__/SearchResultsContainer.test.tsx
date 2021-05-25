192e915b13777262b84802dfa0366bd74ad62557
Niko Lindroos <niko.j.makela@gmail.com>
2021-05-25 14:53:17 +0300

    Added some tests for SearchResultContainer.
    TH-1075.

--- src/domain/eventSearch/searchResultList/__tests__/SearchResultsContainer.test.tsx
+++ src/domain/eventSearch/searchResultList/__tests__/SearchResultsContainer.test.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
 
 import { render, screen } from '../../../../test/testUtils';
+import { EventType } from '../../../event/types';
 import SearchResultsContainer from '../SearchResultsContainer';
 
-it('should return no results text if no events is found', async () => {
+it.each<[number, EventType, string]>([
+  [0, 'event', 'Valitsemillasi hakuehdoilla ei löytynyt yhtään tapahtumaa'],
+  [1, 'event', 'Hakuehdoillasi löytyi vain vähän tapahtumia.'],
+  [4, 'event', 'Hakuehdoillasi löytyi vain vähän tapahtumia.'],
+  [0, 'course', 'Valitsemillasi hakuehdoilla ei löytynyt yhtään harrastusta'],
+  [1, 'course', 'Valitsemillasi hakuehdoilla löytyi vain vähän hakutuloksia.'],
+  [4, 'course', 'Valitsemillasi hakuehdoilla löytyi vain vähän hakutuloksia.'],
+])(
+  'should return the proper results info text if %i %s results are found',
+  (eventsCount, eventType, infoText) => {
+    render(
+      <SearchResultsContainer
+        eventList={<div />}
+        eventsCount={eventsCount}
+        eventType={eventType}
+        loading={false}
+      />
+    );
+    expect(screen.getByText(`${eventsCount} hakutulosta`)).toBeInTheDocument();
+
+    expect(screen.getByText(infoText)).toBeInTheDocument();
+
+    if (eventType === 'course') {
+      expect(
+        screen.queryByRole('button', { name: 'Siirry hakemaan tapahtumia' })
+      ).toBeInTheDocument();
+    } else {
+      expect(
+        screen.queryByRole('button', { name: 'Siirry hakemaan harrastuksia' })
+      ).toBeInTheDocument();
+    }
+  }
+);
+
+it('should not return any results info if more than 5 or more results are found ', async () => {
   render(
     <SearchResultsContainer
       eventList={<div />}
-      eventsCount={0}
+      eventsCount={5}
       eventType="event"
       loading={false}
     />
   );
 
-  expect(
-    screen.getByText(
-      'Valitsemillasi hakuehdoilla ei löytynyt yhtään tapahtumaa'
-    )
-  ).toBeInTheDocument();
-});
+  expect(screen.getByText('5 hakutulosta')).toBeInTheDocument();
 
-// TODO: Maybe add interation test for ResultsInfo here or event better in EventSearchPageContainer :D
+  ['Siirry hakemaan harrastuksia', 'Siirry hakemaan tapahtumia'].forEach(
+    (actionButtonText) => {
+      expect(
+        screen.queryByRole('button', { name: actionButtonText })
+      ).not.toBeInTheDocument();
+    }
+  );
+});
