4c40884ef223ac06f442e95f3c9a80d79b2326c1
Santeri Rusila <santtu_1993@hotmail.com>
2021-06-07 18:25:02 +0300

    Add beta info ribbons

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,35 +5,4 @@ import '@testing-library/jest-dom/extend-expect';
 // Mock scrollTo function
 window.scrollTo = jest.fn();
 
-jest.mock(
-  'popper.js',
-  () =>
-    class Popper {
-      static placements = [
-        'auto',
-        'auto-end',
-        'auto-start',
-        'bottom',
-        'bottom-end',
-        'bottom-start',
-        'left',
-        'left-end',
-        'left-start',
-        'right',
-        'right-end',
-        'right-start',
-        'top',
-        'top-end',
-        'top-start',
-      ];
-
-      constructor() {
-        return {
-          destroy: () => undefined,
-          scheduleUpdate: () => undefined,
-        };
-      }
-    }
-);
-
 jest.setTimeout(30000);
