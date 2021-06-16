c5db797d60ed70a4dcd3c3fe75409b9e41074b9f
Santeri Rusila <santtu_1993@hotmail.com>
2021-06-16 10:19:09 +0300

    Update apollo cache initialization and add comment to eventList pagination handling

--- src/domain/app/apollo/apolloClient.ts
+++ src/domain/app/apollo/apolloClient.ts
@@ -8,47 +8,51 @@ import { onError } from '@apollo/client/link/error';
 import * as Sentry from '@sentry/browser';
 import get from 'lodash/get';
 
-export const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        event(_, { args, toReference }) {
-          return toReference({
-            __typename: 'Keyword',
-            id: args?.id,
-          });
-        },
-        image(_, { args, toReference }) {
-          return toReference({
-            __typename: 'Place',
-            id: args?.id,
-          });
-        },
-        eventList: {
-          keyArgs: (args) =>
-            args
-              ? Object.keys(args).filter((key: string) => key !== 'page')
-              : false,
-          merge(existing, incoming) {
-            return {
-              data: [...(existing?.data ?? []), ...incoming.data],
-              meta: incoming.meta,
-            };
+export const createApolloCache = () =>
+  new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          event(_, { args, toReference }) {
+            return toReference({
+              __typename: 'Keyword',
+              id: args?.id,
+            });
           },
+          image(_, { args, toReference }) {
+            return toReference({
+              __typename: 'Place',
+              id: args?.id,
+            });
+          },
+          eventList: {
+            // Only ignore page argument in caching to get fetchMore pagination working correctly
+            // Other args are needed to separate different serch queries to separate caches
+            // Docs: https://www.apollographql.com/docs/react/pagination/key-args/
+            keyArgs: (args) =>
+              args
+                ? Object.keys(args).filter((key: string) => key !== 'page')
+                : false,
+            merge(existing, incoming) {
+              return {
+                data: [...(existing?.data ?? []), ...incoming.data],
+                meta: incoming.meta,
+              };
+            },
+          },
+          // TODO: finish this when new eventsByIds pagination is merged
+          // eventsByIds: {
+          //   merge(
+          //     existing: EventDetails[] | undefined,
+          //     incoming: EventDetails[]
+          //   ) {
+          //     return [...(existing ?? []), ...incoming];
+          //   },
+          // },
         },
-        // TODO: finish this when new eventsByIds pagination is merged
-        // eventsByIds: {
-        //   merge(
-        //     existing: EventDetails[] | undefined,
-        //     incoming: EventDetails[]
-        //   ) {
-        //     return [...(existing ?? []), ...incoming];
-        //   },
-        // },
       },
     },
-  },
-}).restore(get(window, '__APOLLO_STATE__'));
+  }).restore(get(window, '__APOLLO_STATE__'));
 
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_BASE_URL,
@@ -68,7 +72,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 });
 
 const apolloClient = new ApolloClient({
-  cache,
+  cache: createApolloCache(),
   link: ApolloLink.from([errorLink, httpLink]),
 });
 
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,13 +2,7 @@ import './test/test18nInit';
 import 'jest-axe/extend-expect';
 import '@testing-library/jest-dom/extend-expect';
 
-import { cache } from './domain/app/apollo/apolloClient';
-
 // Mock scrollTo function
 window.scrollTo = jest.fn();
-
-afterEach(() => {
-  cache.reset();
-});
 
 jest.setTimeout(30000);
